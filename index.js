0a8fc9b3cb5ee1135df22d6e95a972914ff05097
1asimdhali <s18237018448@nu.ac.bd>
2024-09-29 11:22:57 +0600

    interactive js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log('conected')
+// getting all the value
+
+
+// add event listener for calculate button
+const calculateButton = document.getElementById('calculate');
+calculateButton.addEventListener('click', function(){
+    const income = parseFloat(document.getElementById("income").value);
+    const sofware = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+    // console.table({income, sofware, courses, internet})
+
+    const totalExpenses = sofware + courses + internet
+    const balance = income - totalExpenses;
+    // console.table({totalExpenses, balance})
+
+
+    const totalExpenseElement = document.getElementById('total-expenses');
+    totalExpenseElement.innerText = totalExpenses.toFixed(2);
+
+    const balanceElement = document.getElementById('balance');
+    balanceElement.innerText = balance.toFixed(2);
+
+
+    // Result
+    const result = document.getElementById('results');
+    result.classList.remove('hidden');
+})
+
+
+// add event listener for savings button
+const calculateSavingsBtn = document.getElementById('calculate-savings');
+calculateSavingsBtn.addEventListener('click', function(){
+    const income = parseFloat(document.getElementById("income").value);
+    const sofware = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+
+
+    const savingsParcentage = parseFloat(document.getElementById('savings').value);
+    // console.log(typeof savingsParcentage);
+
+    const totalExpenses = sofware + courses + internet
+    const balance = income - totalExpenses;
+
+    const savingsAmount = (savingsParcentage * balance) / 100;
+    // console.log(savingsAmount)
+
+    const remainingBalance = balance -savingsAmount;
+
+    const savingsElement = document.getElementById('savings-amount');
+    savingsElement.innerText = savingsAmount.toFixed(2);
+
+    const remainingElement = document.getElementById('remaining-balance');
+    remainingElement.innerText = remainingBalance.toFixed(2);
+})
